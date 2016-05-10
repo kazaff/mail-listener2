32a709780c2ae5a95626c6d1bc3bca065ae2c711
kazaff <edisondik@163.com>
2016-05-10 15:33:00 +0800

    Update index.js
    中文注释

--- index.js
+++ index.js
@@ -34,14 +34,15 @@ function MailListener(options) {
     tlsOptions: options.tlsOptions || {}
   });
 
+  //绑定对应的事件
   this.imap.once('ready', imapReady.bind(this));
   this.imap.once('close', imapClose.bind(this));
   this.imap.on('error', imapError.bind(this));
 }
 
-util.inherits(MailListener, EventEmitter);
+util.inherits(MailListener, EventEmitter);  //这么做，我们的MailListener就拥有创建时间的功能了
 
-MailListener.prototype.start = function() {
+MailListener.prototype.start = function() { //尝试连接邮件服务器
   this.imap.connect();
 };
 
@@ -51,15 +52,15 @@ MailListener.prototype.stop = function() {
 
 function imapReady() {
   var self = this;
-  this.imap.openBox(this.mailbox, false, function(err, mailbox) {
+  this.imap.openBox(this.mailbox, false, function(err, mailbox) { //根据node-imap提供的openBox来请求指定的mailbox
     if (err) {
       self.emit('error', err);
     } else {
       self.emit('server:connected');
-      if (self.fetchUnreadOnStart) {
+      if (self.fetchUnreadOnStart) {  //是否在首次连接建立后直接触发获取所有未读信件
         parseUnread.call(self);
       }
-      self.imap.on('mail', imapMail.bind(self));
+      self.imap.on('mail', imapMail.bind(self));  //每当监听到新邮件时触发的事件及回调
     }
   });
 }
@@ -78,33 +79,33 @@ function imapMail() {
 
 function parseUnread() {
   var self = this;
-  this.imap.search(self.searchFilter, function(err, results) {
+  this.imap.search(self.searchFilter, function(err, results) {  //根据配置的过滤规则来获取信件
     if (err) {
       self.emit('error', err);
     } else if (results.length > 0) {
-      async.each(results, function( result, callback) {
-        var f = self.imap.fetch(result, {
-          bodies: '',
-          markSeen: self.markSeen
+      async.each(results, function( result, callback) { //使用async库提供的并发循环来获取所有目标信件
+        var f = self.imap.fetch(result, {   //获取指定UID的mail
+          bodies: '',   //''相当于获取header + body
+          markSeen: self.markSeen   //是否将该mail设置为已读
         });
-        f.on('message', function(msg, seqno) {
+        f.on('message', function(msg, seqno) {  //由于这里我们的ImapFetch只获取一个特定uid的mail，所以该事件只会触发一次
           var parser = new MailParser(self.mailParserOptions);
           var attributes = null;
 
           parser.on("end", function(mail) {
-            if (!self.mailParserOptions.streamAttachments && mail.attachments && self.attachments) {
+            if (!self.mailParserOptions.streamAttachments && mail.attachments && self.attachments) {  //根据配置判断是否需要将附件保存在指定磁盘位置
               async.each(mail.attachments, function( attachment, callback) {
                 fs.writeFile(self.attachmentOptions.directory + attachment.generatedFileName, attachment.content, function(err) {
                   if(err) {
-                    self.emit('error', err);
-                    callback()
+                    self.emit('error', err);  //触发自定义的error事件
+                    callback()  //任何附件保存异常都忽略了，避免影响并发循环
                   } else {
                     attachment.path = path.resolve(self.attachmentOptions.directory + attachment.generatedFileName);
                     self.emit('attachment', attachment);
                     callback()
                   }
                 });
-              }, function(err){
+              }, function(err){ //处理完所有附件，触发mail事件
                 self.emit('mail', mail, seqno, attributes);
                 callback()
               });
@@ -112,13 +113,13 @@ function parseUnread() {
               self.emit('mail',mail,seqno,attributes);
             }
           });
-          parser.on("attachment", function (attachment) {
+          parser.on("attachment", function (attachment) { //每当解析完一个附件后会触发该事件，注意回调中的参数是一个流
             self.emit('attachment', attachment);
           });
-          msg.on('body', function(stream, info) {
-            stream.pipe(parser);
+          msg.on('body', function(stream, info) { //ImapMessage每当解析完body的完整块后触发该事件
+            stream.pipe(parser);  //以流的方式交给mailparser来解析
           });
-          msg.on('attributes', function(attrs) {
+          msg.on('attributes', function(attrs) {  //ImapMessage解析完消息的完整attributes后触发该事件
             attributes = attrs;
           });
         });
